9a0fae792d574199994e0f5e587d5e8d3aae89de
Adrian Toro <adriandesantis26@gmail.com>
2023-03-01 02:15:53 +0300

    :sparkles: Implement complete meetups CRUD

--- src/app.go
+++ src/app.go
@@ -22,6 +22,13 @@ type Meetup struct {
 	Image       string             `json:"image,omitempty" bson:"image" binding:"required"`
 	Address     string             `json:"address" bson:"address" binding:"required"`
 }
+
+// Custom types
+type Response struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
+
 // Global variables
 var meetupModel *mongo.Collection
 
@@ -54,19 +61,69 @@ func mongoDBConnect(uri string, database string, collection string) *mongo.Colle
 // description: Creates a new meetup
 func createMeetup(c echo.Context) error {
 	fmt.Println("Meetup created")
-	return nil
+
+	meetup := &Meetup{}
+
+	// Get request body
+	if err := c.Bind(meetup); err != nil {
+		return err
+	}
+
+	// Create meetup document
+	response, err := meetupModel.InsertOne(context.TODO(), meetup)
+
+	if err != nil {
+		response := &Response{"500", "Internal Server Error"}
+		return c.JSON(http.StatusInternalServerError, response)
+	}
+
+	// Get created document
+	meetupModel.FindOne(context.TODO(), bson.D{{"_id", response.InsertedID}}).Decode(&meetup)
+
+	return c.JSON(http.StatusCreated, meetup)
 }
 
 // description: Get single meetup
 func getSingleMeetup(c echo.Context) error {
 	fmt.Println("Single meetup returned")
-	return nil
+
+	// Get param id
+	id := c.Param("_id")
+
+	// Convert to mongoID
+	_id, _ := primitive.ObjectIDFromHex(id)
+
+	// Find meeetup
+	var meetup Meetup
+	err := meetupModel.FindOne(context.TODO(), bson.D{{"_id", _id}}).Decode(&meetup)
+
+	// Return error message if meetup is not found
+	if err == mongo.ErrNoDocuments {
+		response := &Response{"404", fmt.Sprintf("No meetup found with given id: %s\n", id)}
+		return c.JSON(http.StatusNotFound, response)
+	}
+
+	return c.JSON(http.StatusOK, meetup)
 }
 
 // description: Get all meetups
 func getAllMeetup(c echo.Context) error {
 	fmt.Println("All meetups returned")
-	return nil
+
+	// Find all meetups
+	var meetups []Meetup
+	cursor, err := meetupModel.Find(context.TODO(), bson.D{})
+
+	err = cursor.All(context.TODO(), &meetups)
+
+	// Return error message if meetup is not found
+
+	if err != nil {
+		response := &Response{"500", "Internal Server Error"}
+		return c.JSON(http.StatusInternalServerError, response)
+	}
+
+	return c.JSON(http.StatusOK, meetups)
 }
 
 // Application entry point
